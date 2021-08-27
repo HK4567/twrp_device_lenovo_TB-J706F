7f6cc5c19f6b494fae256eef7f38f2546dd6c42b
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-08-27 13:45:18 +0800

    10

--- device.mk
+++ device.mk
@@ -14,6 +14,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Display
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 2560
+
 #
 PRODUCT_PLATFORM :=sm6150
 LOCAL_PATH := device/lenovo/J706F
