1cfb4c85e06a7e123f363da4c99310a531345887
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-08-28 15:18:38 +0800

    13

--- device.mk
+++ device.mk
@@ -33,7 +33,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    
+    
 
 PRODUCT_PACKAGES += \
     bootctrl.sm6150
