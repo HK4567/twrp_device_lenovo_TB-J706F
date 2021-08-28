32db69f2c9a973260d1fa1fc5ac3d4d6ed143973
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-08-28 15:39:12 +0800

    13

--- device.mk
+++ device.mk
@@ -1,21 +1,3 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 LOCAL_PATH := device/lenovo/J706F
 
 # A/B
@@ -36,11 +18,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service \
     android.hardware.boot@1.0-impl-wrapper.recovery \
     android.hardware.boot@1.0-impl-wrapper \
-    
-    
-
-PRODUCT_PACKAGES += \
-    bootctrl.sm6150
+    android.hardware.boot@1.0-impl-recovery \
+    bootctrl.sm6150 \
+    bootctrl.sm6150.recovery \
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.sm6150 \
