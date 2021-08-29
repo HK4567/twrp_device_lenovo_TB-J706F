d30608f1bc0752087c2ec413227c1809d20dba73
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-08-29 21:50:34 +0800

    16

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,7 +48,6 @@ export FOX_R11=1
 export FOX_ADVANCED_SECURITY=1
 
 export OF_USE_TWRP_SAR_DETECT=1
-export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
 
 export FOX_USE_ZIP_BINARY="1"
