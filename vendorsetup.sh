6750193af80535aacde39af6b1ff5768c4d8f352
HK不高 <76568542+HK4567@users.noreply.github.com>
2021-09-20 14:09:08 +0800

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,31 @@
+#
+export TARGET_ARCH="arm64"
+
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL="C"
+
+# export OF_USE_MAGISKBOOT=1
+# export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+
+export OF_AB_DEVICE="1"
+
+export OF_STATUS_INDENT_LEFT="48"
+export OF_STATUS_INDENT_RIGHT="48"
+export OF_SCREEN_H=1600
+export OF_SCREEN_W=2560
+export OF_STATUS_H=94
+
+# export OF_USE_TWRP_SAR_DETECT=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+
+# export FOX_USE_ZIP_BINARY="1"
+# export FOX_USE_TAR_BINARY="1"
+# export FOX_ASH_IS_BASH="1"
+# export FOX_REPLACE_BUSYBOX_PS="1"
+# export FOX_USE_BASH_SHELL="1"
+# export FOX_VERSION="R11.0_1.2"
+# export OF_MAINTAINER=NightShadow
+# export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+# export FOX_R11=1
+# export FOX_ADVANCED_SECURITY=1 
+# add_lunch_combo omni_J706F-eng
