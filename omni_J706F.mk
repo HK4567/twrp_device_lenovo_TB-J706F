f414c1906863d9d88a79f8bf9a12c08d895a34ef
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-09-30 11:16:03 +0800

    test

--- omni_J706F.mk
+++ omni_J706F.mk
@@ -9,17 +9,11 @@
 PRODUCT_RELEASE_NAME := J706F
 DEVICE_PATH := device/lenovo/J706F
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from J706F device
 $(call inherit-product, $(DEVICE_PATH)device.mk)
-
-# Inherit some common Omni stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := J706F
