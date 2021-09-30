91c1914d7bacfcc7ce200754a0226b5a5ead1a0f
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-09-30 11:50:54 +0800

    test

--- omni_J706F.mk
+++ omni_J706F.mk
@@ -9,15 +9,14 @@
 PRODUCT_RELEASE_NAME := J706F
 DEVICE_PATH := device/lenovo/J706F
 
-
 # Inherit from J706F device
 $(call inherit-product, $(DEVICE_PATH)device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := J706F
 PRODUCT_NAME := omni_J706F
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo TB-J706F
-PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_MANUFACTURER := lenovo
