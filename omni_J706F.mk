e531dd145fd717739852e378faf552da1d2e195d
HK4567 <76568542+HK4567@users.noreply.github.com>
2021-09-30 12:32:05 +0800

    test

--- omni_J706F.mk
+++ omni_J706F.mk
@@ -10,7 +10,7 @@ PRODUCT_RELEASE_NAME := J706F
 DEVICE_PATH := device/lenovo/J706F
 
 # Inherit from J706F device
-$(call inherit-product, $(DEVICE_PATH)device.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 # $(call inherit-product, vendor/omni/config/common.mk)
 
