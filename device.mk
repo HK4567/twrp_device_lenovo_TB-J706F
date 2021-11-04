3ea9aec4e232641edaac0198309610c739b51075
hk4567 <3144114919@qq.com>
2021-11-04 15:01:34 +0800

    new

--- device.mk
+++ device.mk
@@ -59,4 +59,5 @@ PRODUCT_SOONG_NAMESPACES += \
 #TWRP
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/systemmanifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/system/manifest.xml \
-    $(LOCAL_PATH)/prebuilt/vendormanifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/manifest.xml  
+    $(LOCAL_PATH)/prebuilt/vendormanifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/manifest.xml
+    
