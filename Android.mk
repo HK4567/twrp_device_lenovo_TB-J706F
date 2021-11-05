4e4e35a14f033a73609f7b9a5cdc4dc507519a4c
hk4567 <3144114919@qq.com>
2021-11-05 14:03:40 +0800

    new

--- Android.mk
+++ Android.mk
@@ -26,9 +26,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),J706F)
- 
 include $(call all-makefiles-under,$(LOCAL_PATH)) 
-
-include $(CLEAR_VARS) 
-
 endif
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,21 @@ TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive androidboot.boot_devices=soc/1d84000.ufshc buildvariant=eng
+BOARD_KERNEL_CMDLINE := \
+            androidboot.hardware=qcom \
+            androidboot.memcg=1 \
+            lpm_levels.sleep_disabled=1 \
+            video=vfb:640x400,bpp=32,memsize=3072000 \
+            msm_rtb.filter=0x237 \
+            service_locator.enable=1 \
+            swiotlb=1 \
+            androidboot.usbcontroller=a600000.dwc3 \
+            earlycon=msm_geni_serial,0x880000 \
+            loop.max_part=7 \
+            cgroup.memory=nokmem,nosocket \
+            androidboot.selinux=permissive \
+            androidboot.boot_devices=soc/1d84000.ufshc \
+            buildvariant=eng
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_HEADER_VERSION := 2
