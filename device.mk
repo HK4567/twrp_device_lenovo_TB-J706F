52ffbd29d9d47acbfeb7a5356dd5d9aee6b0b091
hk4567 <3144114919@qq.com>
2021-11-08 20:31:20 +0800

    删除libicui18n.so

--- device.mk
+++ device.mk
@@ -52,8 +52,8 @@ PRODUCT_PACKAGES_ENG += \
     qcom_decrypt_fbe
 
 # Apex libraries
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
