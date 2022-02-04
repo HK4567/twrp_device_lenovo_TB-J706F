457865dfd7678f1f3ec140449e8d5e3c53290e0b
hk4567 <3144114919@qq.com>
2022-02-04 16:27:11 +0800

    新增ashmemd.rc文件，尝试使IAshmemDeviceService工作

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_HOST_PACKAGES += \
     libandroidicu
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
