5c8af0bc8a13d44ed87d359b5138a58ce3be5687
hk4567 <3144114919@qq.com>
2022-02-17 19:19:12 +0800

    解密测试3

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2019 The OmniRom Project
+#
+# Copyright 2020 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,30 +12,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
 #
-# This file is the build configuration for a full Android
-# build for grouper hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps).
-#
+
 
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
- 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
  
 LOCAL_PATH := device/lenovo/J706F
 
 # define hardware platform
 PRODUCT_PLATFORM := sm6150
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 30
 
 # A/B support
 AB_OTA_UPDATER := true
@@ -50,11 +36,13 @@ AB_OTA_PARTITIONS += \
     vendor \
     product \
     vbmeta \
+    vbmeta_system \
     recovery \
     dtbo
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
+    cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
@@ -69,11 +57,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
-  	android.hardware.boot@1.0-impl-wrapper.recovery \
-	  android.hardware.boot@1.0-impl-wrapper \
-	  android.hardware.boot@1.0-impl.recovery \
-	  bootctrl.$(PRODUCT_PLATFORM) \
-	  bootctrl.$(PRODUCT_PLATFORM).recovery
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 # tell update_engine to not change dynamic partition table during updates
 # needed since our qti_dynamic_partitions does not include
@@ -88,8 +76,8 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd 
 
-# qcom decryption
-PRODUCT_PACKAGES_ENG += \
+# qcom standard decryption
+PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
