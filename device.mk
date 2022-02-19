09e5cf4f79d33e287a9dc9e73039dc458953d8e7
hk4567 <3144114919@qq.com>
2022-02-19 16:37:41 +0800

    解密测试12

--- device.mk
+++ device.mk
@@ -83,7 +83,7 @@ PRODUCT_PACKAGES += \
 
 # Apex libraries
 PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+    libandroidicu.so
 
 # OEM otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
